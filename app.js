f3dfb96f618ac7aeefe2864d1d68d7cdc2a96aaa
Nate B <nrbrigmon@gmail.com>
2018-04-16 21:24:05 -0500

    start script?

--- app.js
+++ app.js
@@ -40,16 +40,16 @@ app.use((err, req, res, next) => {
     res.json(err);
 })
 
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
 	// Express will serve up production assets
 	app.use(express.static('client/build'));
-
+	
 	// Express will serve up index.html if
 	//it doesnt understand the route
 	const path = require('path');
 	app.get('*', (req, res) =>
 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
 	);
-}
+// }
 
 module.exports = app;
