71128c63c22ca3cca5b8f773d72f30d34314c163
Nate B <nrbrigmon@gmail.com>
2018-07-03 11:38:14 -0500

    fix

--- client/src/components/metrics/MetricStart.js
+++ client/src/components/metrics/MetricStart.js
@@ -9,7 +9,7 @@ import Typography from '@material-ui/core/Typography';
 import ReactHighcharts from "react-highcharts";
 import Highcharts from 'react-highcharts';
 
-import * as metricConfig from '../metrics/metricConfig';
+import * as MetricConfig from '../metrics/MetricConfig';
 import * as helper from "../../utils/_helperMethods";
 
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
@@ -17,7 +17,7 @@ import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
-import metricList from './metricList';
+import MetricList from './MetricList';
 import { withStyles } from '@material-ui/core/styles';
 
 import MetricStyles from '../../styles/MetricStyles';
@@ -42,7 +42,7 @@ class MetricStart extends Component {
 					<h2>Analyze the Metrics</h2>
 					<p>Click on a metric to view it in full</p>
 					{
-						metricList.map( (item, idx) => {
+						MetricList.map( (item, idx) => {
 							return (
 								<ExpansionPanel key={idx}>
 									<ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
@@ -54,7 +54,7 @@ class MetricStart extends Component {
 										</Typography>
 										<ReactHighcharts 
 											config={
-												metricConfig.chartColumn({
+												MetricConfig.chartColumn({
 													name: item.headline, 
 													data: metricData[item.metric], 
 													categories: devTypes, 
